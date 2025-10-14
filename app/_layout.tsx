71744a2cbefb13e49d0df0e19b81e8b0edd2b418
Arthur Caenen <arthurcaenen@gmail.com>
2025-10-14 15:02:05 +0200

    3

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -12,5 +12,21 @@ export type RootStackParamList = {
 export default function RootLayout() {
   const Stack = createNativeStackNavigator<RootStackParamList>();
 
-  return <>{/* Your code here for the stack navigator */}</>;
+  return <>{/* Your code here for the stack navigator */(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="PostList"
+          component={PostListScreen}
+          options={{ title: "Blog Posts" }}
+        />
+        <Stack.Screen
+          name="PostDetail"
+          component={PostDetailScreen}
+          options={{ title: "Post Details" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}}</>;
 }
