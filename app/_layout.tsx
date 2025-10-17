1ba9bfb57691bdc57697b10de499c2667fa43344
Arthur Caenen <arthurcaenen@gmail.com>
2025-10-17 16:18:13 +0200

    5

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import "react-native-reanimated";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import PostListScreen from "./screens/PostListScreen";
 import PostDetailScreen from "./screens/PostDetailScreen";
+import { Route } from "expo-router/build/Route";
 
 export type RootStackParamList = {
   PostList: undefined;
@@ -12,21 +13,19 @@ export type RootStackParamList = {
 export default function RootLayout() {
   const Stack = createNativeStackNavigator<RootStackParamList>();
 
-  return <>{/* Your code here for the stack navigator */(
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="PostList"
-          component={PostListScreen}
-          options={{ title: "Blog Posts" }}
-        />
-        <Stack.Screen
-          name="PostDetail"
-          component={PostDetailScreen}
-          options={{ title: "Post Details" }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
-}}</>;
-}
+return (
+  <Stack.Navigator initialRouteName="PostList">
+    <Stack.Screen
+      name="PostList"
+      component={PostListScreen}
+      options={{ title: "Posts" }}
+    />
+
+    <Stack.Screen
+      name="PostDetail"
+      component={PostDetailScreen}
+      options={({ route }) => ({ title: route.params.title })}
+    />
+  </Stack.Navigator>
+    );
+;
